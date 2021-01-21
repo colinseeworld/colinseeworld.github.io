1e7ab58fd5974cefb1b6d97afc431946d4012e2a
colinseeworld <48598554+colinseeworld@users.noreply.github.com>
2021-01-21 00:22:38 -0500

    Update script.js

--- Assignment7/script.js
+++ Assignment7/script.js
@@ -5,14 +5,20 @@ function getValue() {
     let height2 = document.getElementById("height2").value;
     let LucasBMI = (weight1 / Math.pow(height1, 2)).toFixed(2);
     let JohnBMI = (weight2 / Math.pow(height2, 2)).toFixed(2);
-    let lucasHigherBMI = LucasBMI > JohnBMI;
+    let lucasHigherBMI = true;
 
-    if (lucasHigherBMI) {
-        window.alert(`Lucas and John are friends, Lucas is ` + height1 + ` meter tall and weight is ` + weight1 + ` in kilograms. John is ` + height2 + ` meter tall and weight is ` + weight2 + ` in kilograms. So Lucas's BMI is:` + LucasBMI + ` and John's BMI is ` + JohnBMI + `. Therefore, Lucas has higher BMI than John, that is ` + LucasBMI);
-    } else if (!lucasHigherBMI) {
-        window.alert(`Lucas and John are friends, Lucas is ` + height1 + ` meter tall and weight is ` + weight1 + ` in kilograms. John is ` + height2 + ` meter tall and weight is ` + weight2 + ` in kilograms. So Lucas's BMI is:` + LucasBMI + ` and John's BMI is ` + JohnBMI + `. Therefore, John has higher BMI than Lucas, that is ` + JohnBMI);
+    if (LucasBMI > JohnBMI) {
+        const summary = document.createElement('p')
+        summary.textContent = `Lucas and John are friends, Lucas is ${height1} meter tall and weight is ${weight1} in kilograms. John is ${height2} meter tall and weight is ${weight2} in kilograms. So Lucas's BMI is:  ${LucasBMI} and John's BMI is ${JohnBMI}. Therefore, Lucas has higher BMI than John? That is ${lucasHigherBMI}.`
+        document.querySelector('body').appendChild(summary)
+    } else if (LucasBMI < JohnBMI) {
+        const summary = document.createElement('p')
+        summary.textContent = `Lucas and John are friends, Lucas is ${height1} meter tall and weight is ${weight1} in kilograms. John is ${height2} meter tall and weight is ${weight2} in kilograms. So Lucas's BMI is:  ${LucasBMI} and John's BMI is ${JohnBMI}. Therefore, Lucas has higher BMI than John? That is ${!lucasHigherBMI}.`
+        document.querySelector('body').appendChild(summary)
     }
     else {
-        window.alert(`They have the same BMI.`)
+        const summary = document.createElement('p')
+        summary.textContent = `They have the same BMI.`
+        document.querySelector('body').appendChild(summary)
     }
 }
